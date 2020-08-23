b77e8214e95cc83a44b74833a9eccc637a90d0c1
Eliza Regas <elizaregas@gmail.com>
2020-08-23 19:00:32 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h2>MERN Template</h2>
+      </div>
+      {/* <Route exact path="/" /> */}
+    </Router>
+  );
+}
+
+export default App;
